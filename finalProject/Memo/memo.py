a4e9e95682369c8e85450f7c04dd7f3deef389da
Ekin Akyürek <akyurekekin@gmail.com>
2016-04-23 09:10:49 +0300

    inputs

--- finalProject/Memo/memo.py
+++ finalProject/Memo/memo.py
@@ -3,6 +3,14 @@ import random
 import math
 args = sys.argv
 
+def splitCharacter(text):
+    newtext = ""
+    for c in text:
+        newtext = newtext + c + " "
+        newtext = newtext[:-1]
+        newtext = newtext + "\n"
+    return newtext
+
 leng = int(args[1])
 size = int(args[2])
 type = str(args[3])
@@ -16,27 +24,21 @@ else:
 
 max_number = math.pow(10, leng + 1)
 min_number = math.pow(10, leng)
-        
-for j in range(size/100):
+
+max_len = int(size/100)
+for len in range(max_len):
     max_number = math.pow(10, leng + 1)                                    
     min_number = math.pow(10, leng)
-
+    
     for i in range(100):   
         var = int(random.uniform(min_number, max_number))
         inputFile.write(str(var)+"\n")
         inputFile.write(str(var)[::-1]+"\n") 
         SpacedinputFile.write(splitCharacter(str(var)))
         SpacedinputFile.write(splitCharacter(str(var)[::-1]))
-
-    if j%100 == 1:
+ 
+    if len%100 == 1:
         leng = leng + 1
+
 inputFile.close()
 SpacedinputFile.close()
-
-def splitCharacter(text):
-    newtext = ""
-    for c in text:
-        newtext = newtext + c + " "
-        newtext = newtext[:-1]
-        newtext = newtext + "\n"
-    return newtext
