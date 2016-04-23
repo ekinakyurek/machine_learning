8f30452656d2b77f4ca194339f024dae3b057450
Ekin Akyürek <akyurekekin@gmail.com>
2016-04-23 20:06:31 +0300

    data

--- finalProject/Memo/memo.py
+++ finalProject/Memo/memo.py
@@ -1,6 +1,7 @@
 import sys
 import random
 import math
+import os
 args = sys.argv
 
 def splitCharacter(text):
@@ -16,13 +17,31 @@ leng = int(args[1])
 size = int(args[2])
 type = str(args[3])
 
-if type == "trn":                                                                         
+if type == "trn":
+    if os.path.exists("SInTrn"):
+        os.remove("SInTrn")
+    if os.path.exists("SOutTrn"):
+        os.remove("SOutTrn")
+    if os.path.exists("InTrn"):
+        os.remove("InTrn")
+    if os.path.exists("InTrn"):
+        os.remove("OutTrn")
+                                                                             
     SpacedinputFile = open('SInTrn','w')                                                  
     SpacedOutputFile = open('SOutTrn', 'w')                                               
-
     inputFile = open('InTrn','w')                                                         
     outputFile = open('OutTrn','w')                                                       
-else:                                                                                     
+else:                 
+    
+    if os.path.exists("SInTst"):
+        os.remove("SInTst")
+    if os.path.exists("SOutTst"):
+        os.remove("SOutTst")
+    if os.path.exists("InTst"):
+        os.remove("InTst")
+    if os.path.exists("InTst"):
+        os.remove("OutTst")
+                                                                        
     SpacedinputFile = open('SInTst','w')                                                  
     SpacedOutputFile = open('SOutTst','w')                                                
     inputFile = open('InTst','w')                                                         
@@ -30,28 +49,58 @@ else:
 
 max_number = math.pow(10, leng + 1)
 min_number = math.pow(10, leng)
+ 
 
-max_len = int(size/100)
-for len in range(max_len):
- #   max_number = math.pow(10, leng + 1)                                    
-  #  min_number = math.pow(10, leng)
+for len in range(int(size/300)):
     
-    for i in range(100):                                                                    
-        var = int(random.uniform(min_number, max_number))                                 
-        inputFile.write(str(var)+"\n")   #input                                           
-        ##inputFile.write(str(var) + str(var)+"\n") #doubled input                           
-        #inputFile.write(str(var)[::-1]+"\n") # reversed input                             
-        outputFile.write(str(var)+"\n") # output                                          
-        ##outputFile.write(str(var)+"\n") # output                                          
-       # outputFile.write(str(var)+"\n") # output                                          
-        SpacedOutputFile.write(splitCharacter(str(var)))                                   
-        SpacedinputFile.write(splitCharacter(str(var)))                          
-       # SpacedinputFile.write(splitCharacter(str(var)[::-1]))
- 
- 
-    if len%2 == 1:
-        leng = leng + 1
-
+    if type == "trn": 
+        max_number = math.pow(10, leng + 1)                                    
+        min_number = math.pow(10, leng)
+        
+        batch = []
+        
+        for i in range(100):                                                                    
+            var = int(random.uniform(min_number, max_number))  
+            
+            inputFile.write(str(var)+"\n")   #input                                          
+            outputFile.write(str(var)+"\n") # output   
+                                                
+            SpacedinputFile.write(splitCharacter(str(var)))  #spaced input        
+            SpacedOutputFile.write(splitCharacter(str(var))) #spaced output
+            
+            batch.append(var)   
+            
+        
+        for j in range(100):      
+                            
+            var = batch[j]                                         
+            inputFile.write(str(var) + str(var)+"\n") #doubled input                                       
+            outputFile.write(str(var)+"\n") # output                                           
+            
+            SpacedinputFile.write(splitCharacter(str(var)+str(var)))   #doubled input                           
+            SpacedOutputFile.write(splitCharacter(str(var)))    #output
+            
+        for j in range(100):                                                                    
+            
+            var = batch[j]                                                
+            inputFile.write(str(var)[::-1]+"\n") # reversed input                                           
+            outputFile.write(str(var)+"\n") # output                                          
+                                
+            SpacedinputFile.write(splitCharacter(str(var)[::-1])) #reversed input
+            SpacedOutputFile.write(splitCharacter(str(var)))   #output
+    
+    
+        if len%2 == 1:
+            leng = leng + 1
+    else:         
+        for i in range(300):                                                                    
+            var = int(random.uniform(min_number, max_number))
+            inputFile.write(str(var)+"\n")   #input                                          
+            outputFile.write(str(var)+"\n") # output   
+                                                    
+            SpacedinputFile.write(splitCharacter(str(var)))  #spaced input        
+            SpacedOutputFile.write(splitCharacter(str(var))) #spaced output
+            
 inputFile.close()
 SpacedinputFile.close()
 outputFile.close()
